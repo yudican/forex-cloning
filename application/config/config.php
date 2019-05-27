d0bfe0c4a318f1749e51740a7f1bfecf11de16dd
yudican <yudhichandra02@gmail.com>
2019-05-27 12:06:20 +0700

    delete disclaimer2

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/forex-cloning/';
+$config['base_url'] = 'http://yudicandra.web.id/forex-clone/';
 
 /*
 |--------------------------------------------------------------------------
